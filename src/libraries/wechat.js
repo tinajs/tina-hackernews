9357429b6a6eda79fab58e7c992685d61e076845
yelo <zhihuzeye@gmail.com>
2017-12-20 21:05:04 +0800

    feat: import tina-loading mixin

--- src/libraries/wechat.js
+++ src/libraries/wechat.js
@@ -4,27 +4,4 @@ import wxio from 'wxio'
 
 wxio.alert = ({ title = '提示', ...options }) => wxio.showModal({ title, ...options, showCancel: false })
 
-wxio.loading = (function () {
-  let stack = 0
-  return function loading (options) {
-    if (options) {
-      wxio.showLoading(options)
-    } else {
-      wxio.showNavigationBarLoading()
-    }
-    ++stack
-    return function loaded () {
-      if (--stack > 0) {
-        return
-      }
-      if (options) {
-        wxio.hideLoading()
-      } else {
-        wxio.hideNavigationBarLoading()
-      }
-      return
-    }
-  }
-})()
-
 export default wxio

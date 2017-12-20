8ffa22bcd9d2e0db95549872d741ed24357d5410
yelo <zhihuzeye@gmail.com>
2017-12-20 17:00:04 +0800

    feat: import wxio

--- src/libraries/wechat.js
+++ src/libraries/wechat.js
@@ -1,93 +1,30 @@
 import Queue from 'p-queue'
 import timeout from 'p-timeout'
+import wxio from 'wxio'
 
-let wechat = Object.assign({}, wx)
+wxio.alert = ({ title = '提示', ...options }) => wxio.showModal({ title, ...options, showCancel: false })
 
-const PROMISIFY_APIS = [
-  'request',
-  'showToast',
-  'showModal',
-  'makePhoneCall',
-  'scanCode',
-  'openSetting',
-  'getSetting',
-  'authorize',
-  'login',
-  'getUserInfo',
-  'requestPayment',
-  'setClipboardData',
-]
-
-const TIMEOUTIFY_APIS = [
-  ['request', 60 * 1000],
-]
-
-const QUEUEIFY_APIS = [
-  ['request', 5],
-]
-
-function promisify (api) {
-  return (options) => new Promise((resolve, reject) => {
-    api(Object.assign({}, options, {
-      success (...args) {
-        resolve(...args)
-      },
-      fail (error) {
-        if (error && error.errMsg) {
-          return reject(new Error(error.errMsg))
-        }
-        reject(new Error(error))
-      },
-    }))
-  })
-}
-
-function queueify (api, concurrency = 1) {
-  const queue = new Queue({ concurrency })
-  return (options) => queue.add(() => api(options))
-}
-
-function timeoutify (api, ms = 5 * 60 * 1000) {
-  return (options) => timeout(api(options), ms)
-}
-
-function initialize () {
-  PROMISIFY_APIS.forEach((name) => {
-    wechat[name] = promisify(wechat[name])
-  })
-  TIMEOUTIFY_APIS.forEach(([name, ms]) => {
-    wechat[name] = timeoutify(wechat[name], ms)
-  })
-  QUEUEIFY_APIS.forEach(([name, concurrency]) => {
-    wechat[name] = queueify(wechat[name], concurrency)
-  })
-
-  wechat.alert = ({ title = '提示', ...options }) => wechat.showModal({ title, ...options, showCancel: false })
-
-  wechat.loading = (function () {
-    let stack = 0
-    return function loading (options) {
-      if (options) {
-        wechat.showLoading(options)
-      } else {
-        wechat.showNavigationBarLoading()
-      }
-      ++stack
-      return function loaded () {
-        if (--stack > 0) {
-          return
-        }
-        if (options) {
-          wechat.hideLoading()
-        } else {
-          wechat.hideNavigationBarLoading()
-        }
+wxio.loading = (function () {
+  let stack = 0
+  return function loading (options) {
+    if (options) {
+      wxio.showLoading(options)
+    } else {
+      wxio.showNavigationBarLoading()
+    }
+    ++stack
+    return function loaded () {
+      if (--stack > 0) {
         return
       }
+      if (options) {
+        wxio.hideLoading()
+      } else {
+        wxio.hideNavigationBarLoading()
+      }
+      return
     }
-  })()
-}
+  }
+})()
 
-initialize()
-
-export default wechat
+export default wxio

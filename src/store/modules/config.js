627201e813cafc0ec5837854fe6168d4ad3a4c48
yelo <zhihuzeye@gmail.com>
2018-02-01 17:21:00 +0800

    feat: fetch menu-ad from config-api

--- src/store/modules/config.js
+++ src/store/modules/config.js
@@ -1,4 +1,5 @@
 import uniq from 'uniq'
+import map from 'just-map-object'
 import types from '../types'
 import {
   fetchConfig,
@@ -6,9 +7,32 @@ import {
 
 const initialState = {
   github: false,
+  ads: {
+    menu: {
+      visible: true,
+      thumbnail: '',
+      message: {
+        type: 'link',
+        link: {
+          title: 'HackerNews 热点 @ GitHub',
+          url: 'https://github.com/tinajs/tina-hackernews',
+          thumbnail: 'https://github.com/tinajs/assets/raw/master/images/showcases/hackernews.png',
+        },
+      },
+    },
+  }
 }
 
-const getters = {}
+const getters = {
+  ads: (state) => {
+    return map(state.ads, (key, ad) => {
+      return {
+        ...ad,
+        message: JSON.stringify(ad.message),
+      }
+    })
+  },
+}
 
 const actions = {
   fetchConfig ({ commit, state }) {
